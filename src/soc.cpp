a98f8e6a65a59e529b6729ca9fa4edbb3946aed4
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-05-21 11:22:33 -0300

    Atualização do código

--- src/soc.cpp
+++ src/soc.cpp
@@ -4,21 +4,23 @@
 
 #include "xtensor/xarray.hpp"
 #include <xtensor/xhistogram.hpp>
+#include "xtensor/xadapt.hpp"
 
-xt::xarray<double> soc(xt::xarray<double>& soc, std::size_t binSize = 50)
+xt::xarray<double> soc(xt::xarray<double>& vec, std::size_t binSize = 50)
 {
-    std::size_t n = soc.size();
+    std::size_t n = vec.size();
     std::vector<std::size_t> shape = { 1, n };
 
-    auto mean = xt::mean(soc);
-    auto var = xt::variance(soc);
-    auto std = xt::stddev(soc);
+    auto mean = xt::mean(vec);
+    auto std = xt::stddev(vec);
 
-    std::vector<double> vec;
+    std::vector<double> gamma;
 
-    for (auto data: soc)
+    for (auto data: vec)
     {
-        vec.push_back(((data - mean) / std)(0));
+        gamma.push_back(((data - mean) / std)(0));
     }
+
+    auto histData = xt::adapt(gamma, shape);
 
 }
